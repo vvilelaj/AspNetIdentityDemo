ce0afcd5b92d4b40b200f3dc8fed53df3b0e32e2
Victor Vilela <vilela_victor@hotmail.com>
2018-09-11 11:08:06 -0500

    block /Home/Contact to authenticated users

--- AspNetIdentityDemo.Web/Controllers/HomeController.cs
+++ AspNetIdentityDemo.Web/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace AspNetIdentityDemo.Web.Controllers
 {
+    
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -20,6 +21,7 @@ namespace AspNetIdentityDemo.Web.Controllers
             return View();
         }
 
+        [Authorize]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
